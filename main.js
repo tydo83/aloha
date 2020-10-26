c9f16b8ca892be9f75994409c4d5bff1eb0dee30
Timothy Do <tydo83@gmail.com>
2020-10-26 10:25:09 -0400

    fixed main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const getAloha = require('./get-aloha.js');
 
 const userLanguage = process.argv[2];
 const language = getAloha(userLanguage);
-console.log(language.greeting);
-console.log(language.farewell);
-
+if(language === null) {
+    console.log('Your language is not in our database');
+}
+else {
+    console.log(language.greeting);
+    console.log(language.farewell);
+}
